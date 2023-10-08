3e0dfe9b77bce0cb3a7cc51d029cc5f23a703386
Anik-Dv <freelanceranik50@gmail.com>
2023-10-08 23:23:57 +0600

    add some mgs

--- app.java
+++ app.java
@@ -2,5 +2,6 @@ public class app{
     public static void main(String[] args) {
         System.out.println("Hello From Main App");
         System.out.println("This app deploy jenkins");
+        System.out.println("Jenkins deploy this app, app also say hello");
     }
 }
--- test.java
+++ test.java
@@ -3,6 +3,7 @@ public class test {
         
         System.out.println("hello From Test Class");
         System.out.println("this is deploy with jenkins");
+        System.out.println("Jenkins deploy this app, test class say hello!");
 
     }
 }
